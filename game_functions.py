f4589765a5baf03dd02040c11b8d3e5eb800aa06
Shaun Coulter <shaundcoulter@gmail.com>
2019-03-27 10:39:33 -0400

    new update, added game_functions

---
+++ game_functions.py
@@ -0,0 +1,32 @@
+import sys
+
+import pygame
+
+def check_events(ship):
+    """respond to keypress and mouse events"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            # Move ship right and left.
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+
+        elif event.type == pygame.KEYUP:
+            # Stop moving ship right and left.
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
+
+def update_screen(ai_settings, screen, ship):
+    """Update images on screen and flip to new screen"""
+    # Redraw the screen during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
